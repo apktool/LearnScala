1d8d97eeaa068965337d4be9cc4622519c4e8df7
apktool <apktoolOo@gmail.com>
2017-10-04 12:17:33 +0800

    sequence comprehensions

---
+++ 10.04.02.scala
@@ -0,0 +1,24 @@
+/* 
+ * sequence comprehensions
+ * http://docs.scala-lang.org/tour/sequence-comprehensions.html
+ *
+ * Note
+ * for (enumerators) yield e
+ */
+
+
+object ComprehensionTest1 extends App {
+  def even(from: Int, to: Int): List[Int] =
+    for(i <- List.range(from, to) if i % 2 == 0) yield i
+  Console.println(even(0, 20))
+
+  /*---------------*/
+
+  def foo(n: Int, v: Int) =
+    for (i <- 0 until n; j <- i until n if i + j == v) yield (i, j);
+
+  foo(20, 32) foreach {
+    case (i, j) =>
+      println(s"($i, $j)")
+  }
+}
